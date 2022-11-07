b2d5b3f753b6aeacaedea087fd4c0460ebf9e715
Filipp Pinskiy <pinskiy.fs@ya.ru>
2022-11-07 21:27:53 +0300

    Выполнено задание. Модуль 4. Урок 1.

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+const nameProduct = 'Samsung s20';
+console.log('Наименование товара: ', nameProduct);
+
+const quantityProduct = 11 ;
+const categoryProduct = 'Сматфоны';
+const priseProduct = 700;
+
+
+//? Дополнительное задание
+
+const total = quantityProduct * priseProduct;
+console.log('Общая сумма: ', total);
