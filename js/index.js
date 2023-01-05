faf51731ba3dc02a833af0b046cba50f69497bc1
unknown <Pinsky-FS@yandex.ru>
2023-01-05 17:39:33 +0300

    Выполнил задание. Модуль 5. Урок 2.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
+'use strict';
 
-const nameProduct = 'Samsung s20';
-console.log('Наименование товара: ', nameProduct);
+const title = document.querySelector('.form__title');
+const cardId = document.querySelector('.title--id');
+const btnId = document.querySelector('.title__btn--id');
+const form = document.querySelector('.form');
+const checkbox = document.querySelector('.checkbox__input');
+const inputCheckbox = document.querySelector('.form__input_disabled');
+const totalPrice = document.querySelector('.total-cost__text');
 
-const quantityProduct = 11 ;
-const categoryProduct = 'Сматфоны';
-const priseProduct = 700;
 
 
-//? Дополнительное задание
-
-const total = quantityProduct * priseProduct;
-console.log('Общая сумма: ', total);
